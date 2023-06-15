786a212f06d28686a7db676f5d79c4ca8fceb012
NasimBorazjanizadeh <52820699+NasimBorazjanizadeh@users.noreply.github.com>
2023-06-15 16:19:46 -0400

    Update train_gpt2_dp_ss.sh
    train with 2 gpus

--- experiments/gpt2_dp/train_gpt2_dp_ss.sh
+++ experiments/gpt2_dp/train_gpt2_dp_ss.sh
@@ -5,6 +5,6 @@
 #SBATCH --mem=100G           # Memory pool for all cores (see also --mem-per-cpu)
 #SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
-#SBATCH --gres=gpu:4        # Request two GPUs
+#SBATCH --gres=gpu:2        # Request two GPUs
 
 python train_gpt2_dp.py 
