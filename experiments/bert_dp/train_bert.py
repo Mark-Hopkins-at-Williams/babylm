e0f8ee4264d85e1d5edd8e463ef98d9e3ac6d27a
NasimBorazjanizadeh <52820699+NasimBorazjanizadeh@users.noreply.github.com>
2023-06-17 20:12:52 -0400

    updated directory

--- experiments/bert_dp/train_bert.py
+++ experiments/bert_dp/train_bert.py
@@ -25,7 +25,7 @@ model = BertForMaskedLM(config=model_config)
 eval_logging_ckp_steps = 500
 
 args = TrainingArguments(
-    output_dir="bert-dp",
+    output_dir="bert-dp-second",
     per_device_train_batch_size=32,
     per_device_eval_batch_size=32,
     evaluation_strategy="steps",
