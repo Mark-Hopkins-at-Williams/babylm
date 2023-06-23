e4ec0b5361a0732c9548d9c87cf41271f9e58300
NasimBorazjanizadeh <nb11@williams.edu>
2023-06-22 22:29:35 -0400

    added experiment for simcse

---
+++ experiments/SimCSE/bert_base_usup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# In this example, we show how to train SimCSE on unsupervised Wikipedia data.
+# If you want to train it with multiple GPU cards, see "run_sup_example.sh"
+# about how to use PyTorch's distributed data parallel.
+
+python3 train.py \
+    --model_name_or_path bert-base-uncased \
+    --train_file /home/nasimb/SimCSE/simcse/simple_wikipedia.txt \
+    --output_dir result/unsup-simcse-bert-base \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 64 \
+    --learning_rate 3e-5 \
+    --max_seq_length 32 \
+    --evaluation_strategy steps \
+    --eval_steps 125 \
+    --pooler_type cls \
+    --mlp_only_train \
+    --overwrite_output_dir \
+    --temp 0.05 \
+    --do_train \
+    --do_eval \
+    --fp16 \
+    "$@"
