36205a28f907af688ca0bd5f2d640f5685345d50
NasimBorazjanizadeh <nb11@williams.edu>
2023-06-24 18:30:12 -0400

    fixed bugs infinetuning simcse bert concat 3 loop

---
+++ experiments/local-model-finetune-post-simcse/train_bert_finetune_simcse_truncate_ss.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH -c 1                # Number of cores (-c)
+#SBATCH -t 0-12:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -p dl               # Partition to submit to
+#SBATCH --mem=100G           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
+#SBATCH --gres=gpu:2        # Request two GPUs
+
+python /home/nasimb/babylm/experiments/local-model-finetune-post-simcse/train_bert_finetune_simcse_truncate.py
