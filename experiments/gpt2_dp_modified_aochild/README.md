bce245420dd961ea71b4a5a73d6b0f80e5777a77
NasimBorazjanizadeh <nb11@williams.edu>
2023-06-23 22:43:38 -0400

    added experiment for training gpt-2 dp on 9 datasets and aochild modified

---
+++ experiments/gpt2_dp_modified_aochild/README.md
@@ -0,0 +1,10 @@
+Notes:
+
+- Tokenizer for this experiment is imported from huggingface
+- trains the model with 9 datasets in strict small, all but aochild
+- aochildes dataset modified to start from line 20000
+- uses DP for augmenting the tokenized sequences
+
+Run the following on Appa:
+    sbatch /home/nasimb/babylm/experiments/gpt2_dp_modified_aochild/train_gpt2_dp_mod_aochild_ss.sh
+
---
+++ experiments/gpt2_dp_modified_aochild/train_gpt2_dp_mod_aochild_ss.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH -c 1                # Number of cores (-c)
+#SBATCH -t 0-12:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -p dl               # Partition to submit to
+#SBATCH --mem=100G           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
+#SBATCH --gres=gpu:2        # Request two GPUs
+
+python /home/nasimb/babylm/experiments/gpt2_dp_modified_aochild/train_gpt2_dp_mod_aochild.py
