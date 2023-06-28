eee465a2c52437611d850cd3b3c7305bb5be91ce
NasimBorazjanizadeh <nb11@williams.edu>
2023-06-28 03:57:18 -0400

    cl rarity

--- experiments/gpt2_dp_cl/tokenize_and_data_gpt2_dp_cl.py
+++ experiments/gpt2_dp_cl/tokenize_and_data_gpt2_dp_cl.py
@@ -26,7 +26,7 @@ def create_multiple_files_dataset_dict():
                'children_stories', 'cbt', 'gutenberg', 
                'qed', 'simple_wikipedia', 'switchboard', 'wikipedia']
     #train_corpora = [f'../babylm_data/babylm_10M/{corpus}.train' for corpus in corpora]
-    train_corpora = ['/mnt/storage/nasimb/babylm/train_data_cl_length.txt']
+    train_corpora = ['/mnt/storage/nasimb/babylm/train_data_cl_rarity.txt']
     dev_corpora = [f'../babylm_data/babylm_dev/{corpus}.dev' for corpus in corpora]
     return create_dataset_dict(train_corpora, dev_corpora)
    
