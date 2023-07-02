48564bddd58ab225820fa99f8e384d74e7aaf446
NasimBorazjanizadeh <nb11@williams.edu>
2023-07-02 05:53:50 -0400

    gpt2 cl rarity 150k

--- experiments/gpt2_dp_cl/tokenize_and_data_gpt2_dp_cl.py
+++ experiments/gpt2_dp_cl/tokenize_and_data_gpt2_dp_cl.py
@@ -27,7 +27,7 @@ def create_multiple_files_dataset_dict():
                'children_stories', 'cbt', 'gutenberg', 
                'qed', 'simple_wikipedia', 'switchboard', 'wikipedia']
     #train_corpora = [f'../babylm_data/babylm_10M/{corpus}.train' for corpus in corpora]
-    train_corpora = ['/mnt/storage/nasimb/babylm/train_data_length_cl_sampling_4_275k.txt']
+    train_corpora = ['/mnt/storage/nasimb/babylm/train_data_rarity_cl_sampling_4_150k.txt']
     dev_corpora = [f'../babylm_data/babylm_dev/{corpus}.dev' for corpus in corpora]
     test_corpora = [f'../babylm_data/babylm_test/{corpus}.test' for corpus in corpora]
     return create_dataset_dict(train_corpora, dev_corpora, test_corpora)
--- experiments/gpt2_dp_cl/train_gpt2_dp_cl.py
+++ experiments/gpt2_dp_cl/train_gpt2_dp_cl.py
@@ -31,7 +31,7 @@ model = GPT2LMHeadModel(config)
 eval_logging_ckp_steps = 500
 
 args = TrainingArguments(
-    output_dir="gpt2-cl-length-sampling-4",
+    output_dir="gpt2-cl-rarity-sampling-4",
     per_device_train_batch_size=32,
     per_device_eval_batch_size=32,
     evaluation_strategy="steps",
