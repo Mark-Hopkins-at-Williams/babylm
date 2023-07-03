883574e66bf8bd122fb0d86f26b7743a1a63b3c5
NasimBorazjanizadeh <nb11@williams.edu>
2023-07-03 01:27:56 -0400

    experiment for cl rarity with modified datasets

--- experiments/gpt2_dp_cl/create_train_corpora_cl_length_rarity_datasets.py
+++ experiments/gpt2_dp_cl/create_train_corpora_cl_length_rarity_datasets.py
@@ -19,7 +19,17 @@ def read_lines(filenames):
                 if len(line) > 0 and line[0] == "-":
                     line = line[1:]
                 if len(line) > 0 and bar_count < 2 and bar_count + exclamation_count < 2:
-                    yield {'text': line}           
+                    yield {'text': line}   
+                """
+                #gutenberg
+                line = line.strip()
+                bar_count =  line.count('|')
+                exclamation_count = line.count('!')
+                line = line.replace("= = =", "")
+                gutenberg_line += line
+                if len(line) == 0 and len(gutenberg_line) > 1:
+                    yield {'text': gutenberg_line}         
+                    gutenberg_line = ""  """        
 
 def create_dataset(filenames):
     return Dataset.from_generator(lambda: read_lines(filenames))
@@ -30,9 +40,7 @@ def create_dataset_dict(train_file_names):
     return result
 
 def create_multiple_files_dataset_dict():
-    corpora = ['aochildes_modified_length_14k', 'bnc_spoken', 'open_subtitles',
-               'children_stories', 'cbt_modified_rarity_2k', 'gutenberg_modified_rarity_2.2k_1k', 
-               'qed', 'simple_wikipedia', 'switchboard', 'wikipedia']
+    corpora = ['cbt']
     train_corpora = [f'/mnt/storage/nasimb/babylm_data/babylm_10M/{corpus}.train' for corpus in corpora]
     return create_dataset_dict(train_corpora)
       
@@ -63,29 +71,27 @@ for seq in list_tokenized_seqs:
 dict_ind_token_rarity = {i:(sum([token_counts[token] for token in list_tokenized_seqs[i]]) 
                             / len(list_tokenized_seqs[i])) 
                          for i in range(len(list_tokenized_seqs))}
-sorted_indecies = sorted(dict_ind_token_rarity, key=lambda k:dict_ind_token_rarity[k], reverse=True)
+
+#calculate the order of raw sentences based on token length
+tokenized_seq_lengths = [len(x) for x in tokenized_datasets["train"]["input_ids"]]
+dict_ind_token_length = {i:tokenized_seq_lengths[i] for i in range(len(tokenized_seq_lengths))}
+
+
+#print({k: dict_ind_token_length[k] for k in list(dict_ind_token_length.keys())[:100]})
+#print({k: dict_ind_token_rarity[k] for k in list(dict_ind_token_rarity.keys())[:100]})
+
+sorted_indecies = sorted(dict_ind_token_rarity, key=lambda k:(dict_ind_token_rarity[k]))
 
 #reorder the raw datatset
 list_train_dataset_raw = list(raw_datasets["train"]["text"])
 sorted_list_train_dataset_raw = [list_train_dataset_raw[i] for i in sorted_indecies]
 
-#remove repeating instances from the list preserving the order
+#remove repeating instances from the list preserving the order, and cut
 sorted_list_train_dataset_raw = list(dict.fromkeys(sorted_list_train_dataset_raw))
-    
-#no cuts in the rarity ordered dataset
 
-#sampling with the square root function
-batch_size = 32
-t_competent = 138000 #doubled #5 epochs to competence, 800000 sentences in batches of 32 
-c0_squared = (1/t_competent)**2
-num_sent = len(sorted_list_train_dataset_raw)
-with open('train_data_rarity_cl_6_modified_138k.txt', 'w') as f:
-    for t in tqdm(range(t_competent)):
-        c_sqrt = min(1, math.sqrt(t * ((1 - c0_squared) / t_competent) + c0_squared))
-        max_ind = max(batch_size, int(c_sqrt * num_sent))
-        sample_inds = np.random.randint(low = 0,high=max_ind,size=batch_size)
-        for ind in sample_inds:
-            f.write(f"{sorted_list_train_dataset_raw[ind]}\n")
-    f.close()
-        
+with open('/mnt/storage/nasimb/babylm_data/babylm_10M/cbt_modified.train', 'w') as f:
+    for sent in sorted_list_train_dataset_raw:
+        f.write(f"{sent}\n")
+    
+
 
