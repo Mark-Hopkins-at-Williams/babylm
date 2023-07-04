49436cbac9d505d222c3eced4132ccb96cb4effc
NasimBorazjanizadeh <nb11@williams.edu>
2023-07-04 03:57:56 -0400

    gpt2 concat mod datasets 2 layered rarity

--- experiments/gpt2_concat/train_gpt2_concatenation.py
+++ experiments/gpt2_concat/train_gpt2_concatenation.py
@@ -32,7 +32,7 @@ model = GPT2LMHeadModel(config)
 eval_logging_ckp_steps = 500
 
 args = TrainingArguments(
-    output_dir="gpt2-concat-gutenberg-fixed",
+    output_dir="gpt2-concat-mod-datasets-rarity2",
     per_device_train_batch_size=32,
     per_device_eval_batch_size=32,
     evaluation_strategy="steps",
