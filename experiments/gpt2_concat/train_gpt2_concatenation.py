81d6eeed937ae33fdd4566eca4b9cf0e8f809461
NasimBorazjanizadeh <nb11@williams.edu>
2023-07-05 15:52:46 -0400

    gpt2 concat gutenberg rarity 2.2k 1k

--- experiments/gpt2_concat/train_gpt2_concatenation.py
+++ experiments/gpt2_concat/train_gpt2_concatenation.py
@@ -32,7 +32,7 @@ model = GPT2LMHeadModel(config)
 eval_logging_ckp_steps = 500
 
 args = TrainingArguments(
-    output_dir="gpt2-concat-aochildes-16k",
+    output_dir="gpt2-concat-gutenberg-2p2k-1k",
     per_device_train_batch_size=32,
     per_device_eval_batch_size=32,
     evaluation_strategy="steps",
