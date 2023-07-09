2a718a223f1c6e4c75bea9f8a0fdad460eac5e32
NasimBorazjanizadeh <nb11@williams.edu>
2023-07-09 03:27:18 -0400

    gpt2 concat modified giuten to remove refrences code for dataset prep in next commit

--- experiments/gpt2_concat3/train_gpt2_concatenation.py
+++ experiments/gpt2_concat3/train_gpt2_concatenation.py
@@ -32,7 +32,7 @@ model = GPT2LMHeadModel(config)
 eval_logging_ckp_steps = 500
 
 args = TrainingArguments(
-    output_dir="gpt2-concat-guten-rarity-5k-2p5k",
+    output_dir="gpt2-concat-guten-mod-rarity-1k-p1k",
     per_device_train_batch_size=32,
     per_device_eval_batch_size=32,
     evaluation_strategy="steps",
--- experiments/gpt2_concat3/train_gpt2_concatenation_ss.sh
+++ experiments/gpt2_concat3/train_gpt2_concatenation_ss.sh
@@ -9,4 +9,4 @@
 
 python /mnt/storage/nasimb/babylm/experiments/gpt2_concat3/train_gpt2_concatenation.py
 cd ../evaluation-pipeline
-python babylm_eval.py /mnt/storage/nasimb/babylm/gpt2-concat-guten-rarity-5k-2p5k decoder
+python babylm_eval.py /mnt/storage/nasimb/babylm/gpt2-concat-guten-mod-rarity-1k-p1k decoder
