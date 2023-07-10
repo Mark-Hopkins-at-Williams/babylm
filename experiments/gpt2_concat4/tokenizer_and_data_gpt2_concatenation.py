b4bd62e4685fb757f79d503947aa7a99699bfe6f
NasimBorazjanizadeh <nb11@williams.edu>
2023-07-10 04:41:05 -0400

    gpt2 concat with new mod datasets with text processing

--- experiments/gpt2_concat4/tokenizer_and_data_gpt2_concatenation.py
+++ experiments/gpt2_concat4/tokenizer_and_data_gpt2_concatenation.py
@@ -27,14 +27,14 @@ def create_multiple_files_dataset_dict():
                'switchboard', 'children_stories', 'bnc_spoken', 'simple_wikipedia',
                'wikipedia', 'cbt', 'gutenberg',]
     
-    train_corpora = ['aochildes', 'bnc_spoken', 'open_subtitles',
-               'children_stories', 'cbt', 'guten_mod_rm_ref_2k_rarity_2p5k_p13k', 
-               'qed', 'simple_wikipedia', 'switchboard', 'wikipedia']
+    train_corpora = ['aochildes_mod_no_repeating_sub', 'bnc_spoken', 'open_subtitles',
+               'children_stories', 'cbt_mod_formatting_iorder', 'guten_mod_rm_refrences_1p7k', 
+               'qed', 'simple_wiki_mod', 'switchboard', 'wikipedia']
     
     train_corpora = [f'../babylm_data/babylm_10M/{corpus}.train' for corpus in train_corpora]
     dev_corpora = [f'../babylm_data/babylm_dev/{corpus}.dev' for corpus in corpora]
     test_corpora = [f'../babylm_data/babylm_test/{corpus}.test' for corpus in corpora]
-    return create_dataset_dict(train_corpora, dev_corpora, train_corpora)
+    return create_dataset_dict(train_corpora, dev_corpora, test_corpora)
 
 
 CONTEXT_LENGTH = 128
--- experiments/gpt2_concat4/train_gpt2_concatenation.py
+++ experiments/gpt2_concat4/train_gpt2_concatenation.py
@@ -32,7 +32,7 @@ model = GPT2LMHeadModel(config)
 eval_logging_ckp_steps = 500
 
 args = TrainingArguments(
-    output_dir="gpt2-concat-guten-mod-rm-ref-2k-rarity-2p5k-p13k",
+    output_dir="gpt2-cocnat-mod-datasets-txt-processing",
     per_device_train_batch_size=32,
     per_device_eval_batch_size=32,
     evaluation_strategy="steps",
