20bf81b9bd950c099dfa28ba4a912d3988e5708c
NasimBorazjanizadeh <nb11@williams.edu>
2023-07-11 00:05:44 -0400

    error in 3d8dff8

--- experiments/gpt2_dp/train_gpt2_dp.py
+++ experiments/gpt2_dp/train_gpt2_dp.py
@@ -32,7 +32,7 @@ model = GPT2LMHeadModel(config)
 eval_logging_ckp_steps = 500
 
 args = TrainingArguments(
-    output_dir="gpt2-dp-mod-datasets-txt-processing",
+    output_dir="gpt2-dp-guten-rarity-all-5k-2p5k",
     per_device_train_batch_size=32,
     per_device_eval_batch_size=32,
     evaluation_strategy="steps",
