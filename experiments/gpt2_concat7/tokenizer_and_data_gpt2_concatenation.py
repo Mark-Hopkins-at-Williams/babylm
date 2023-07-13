131cc983f958aef11cca279727c19a830797a485
NasimBorazjanizadeh <nb11@williams.edu>
2023-07-13 01:51:30 -0400

    guten rarity no cut corrected

--- experiments/gpt2_concat7/tokenizer_and_data_gpt2_concatenation.py
+++ experiments/gpt2_concat7/tokenizer_and_data_gpt2_concatenation.py
@@ -29,7 +29,7 @@ def create_multiple_files_dataset_dict():
     
     train_corpora = ['aochildes', 'bnc_spoken', 'open_subtitles',
                'children_stories', 'cbt', 'guten_rarity_no_cut', 
-               'qed', 'simple_wiki_mod', 'switchboard', 'wikipedia']
+               'qed', 'simple_wikipedia', 'switchboard', 'wikipedia']
     
     train_corpora = [f'../babylm_data/babylm_10M/{corpus}.train' for corpus in train_corpora]
     #train_corpora = ['/mnt/storage/nasimb/babylm_data/babylm_10M/all_mod_datasets_rarity_all_iorder_no_cut_repetition.train']
