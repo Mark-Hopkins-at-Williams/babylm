666d990624d9d4a04fb273251f234413ad9829f5
NasimBorazjanizadeh <nb11@williams.edu>
2023-07-16 22:13:28 -0400

    glue eval for guten rarity all end 2p5k

---
+++ experiments/glue_eval/glue_eval2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#SBATCH -c 1                # Number of cores (-c)
+#SBATCH -t 0-20:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -p dl               # Partition to submit to
+#SBATCH --mem=100G           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
+#SBATCH --gres=gpu:1        # Request one GPUs
+
+cd ../evaluation-pipeline
+./finetune_all_tasks.sh '/mnt/storage/nasimb/babylm/gpt2-concat-guten-rarity-all-end-2p5k'
