993048457cbfdcfe84568485ee9db5f987ab01ab
NasimBorazjanizadeh <nb11@williams.edu>
2023-07-21 17:10:12 -0400

    guten cbt norm rarity neg log rarity

--- experiments/data-cleaning/data_sorting_norm_rarity_neg_log_rarity.py
+++ experiments/data-cleaning/data_sorting_norm_rarity_neg_log_rarity.py
@@ -31,7 +31,7 @@ def create_dataset_dict(train_file_names):
 
 def create_multiple_files_dataset_dict(one_dataset):
     if one_dataset:
-        corpora = ['gutenberg_fixed', ]
+        corpora = ['gutenberg_fixed', 'cbt']
     else:
         corpora = ['bnc_spoken', 'open_subtitles', 'aochildes', 
                'children_stories', 'cbt', 'gutenberg_fixed', 
@@ -127,9 +127,9 @@ else:
 sorted_list_train_dataset_raw = [list_train_dataset_raw[i] for i in sorted_indecies]
 
 #remove repeating instances from the list preserving the order, and cut
-sorted_list_train_dataset_raw = list(dict.fromkeys(sorted_list_train_dataset_raw))[1500:19500]
+sorted_list_train_dataset_raw = list(dict.fromkeys(sorted_list_train_dataset_raw))
 
-with open('/mnt/storage/nasimb/babylm_data/babylm_10M/guten_norm_rarity_neg_log_rarity_1p5k_end_19p5k.train', 'w') as f:
+with open('/mnt/storage/nasimb/babylm_data/babylm_10M/guten_cbt_norm_rarity_neg_log_rarity.train', 'w') as f:
     for sent in sorted_list_train_dataset_raw:
         f.write(f"{sent}\n")
         
