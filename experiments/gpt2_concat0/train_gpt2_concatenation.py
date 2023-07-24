6c852d74190de806ca1e61c85a6e4981556fab4f
NasimBorazjanizadeh <nb11@williams.edu>
2023-07-23 20:53:58 -0400

    all datasets gutenberg not modified

--- experiments/gpt2_concat0/train_gpt2_concatenation.py
+++ experiments/gpt2_concat0/train_gpt2_concatenation.py
@@ -74,7 +74,7 @@ model.config.pad_token_id = model.config.eos_token_id
 eval_logging_ckp_steps = 500
 
 args = TrainingArguments(
-    output_dir="guten-rarity-neg-log-rarity-end-19p1k",
+    output_dir="all-base-guten-no-modified",
     per_device_train_batch_size=32,
     per_device_eval_batch_size=32,
     evaluation_strategy="steps",
