00313e973a903d9172e548a4f69ab8aacb84d496
NasimBorazjanizadeh <nb11@williams.edu>
2023-07-25 20:54:00 -0400

    cl log rarity 70M tokens 280k samples

--- experiments/gpt2_concat018/train_gpt2_concatenation_ss.sh
+++ experiments/gpt2_concat018/train_gpt2_concatenation_ss.sh
@@ -9,6 +9,6 @@
 
 python /mnt/storage/nasimb/babylm/experiments/gpt2_concat018/train_gpt2_concatenation.py
 cd ../evaluation-pipeline
-python babylm_eval.py /mnt/storage/nasimb/babylm/guten-no-merge-len decoder
+python babylm_eval.py /mnt/storage/nasimb/babylm/cl-log-rarity-280k decoder
 
 #./finetune_all_tasks.sh /mnt/storage/nasimb/babylm/guten-rarity-all-2p5k-new-loop-pad
