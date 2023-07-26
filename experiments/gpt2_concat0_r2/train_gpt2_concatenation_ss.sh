4772ec1ff02cdff4758f46c0acceef34ea8afa91
NasimBorazjanizadeh <nb11@williams.edu>
2023-07-26 05:47:34 -0400

    bnc log rarity

---
+++ experiments/gpt2_concat0_r2/train_gpt2_concatenation_ss.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH -c 1                # Number of cores (-c)
+#SBATCH -t 0-16:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -p dl               # Partition to submit to
+#SBATCH --mem=100G           # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
+#SBATCH --gres=gpu:2        # Request two GPUs
+
+python /mnt/storage/nasimb/babylm/experiments/gpt2_concat0_r2/train_gpt2_concatenation.py
